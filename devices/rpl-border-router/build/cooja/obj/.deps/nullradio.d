47ceaa9d2e1b2c2f161ba697674a290c4bd7a9d2
Riccardo Bertini <m_bertini@hotmail.com>
2023-05-29 05:37:26 -0400

    First Initialization

---
+++ devices/rpl-border-router/build/cooja/obj/.deps/nullradio.d
@@ -0,0 +1,7 @@
+build/cooja/obj/nullradio.o: /home/user/contiki-ng/os/dev/nullradio.c \
+ /home/user/contiki-ng/os/services/rpl-border-router/module-macros.h \
+ /home/user/contiki-ng/os/dev/nullradio.h \
+ /home/user/contiki-ng/os/dev/radio.h
+/home/user/contiki-ng/os/services/rpl-border-router/module-macros.h:
+/home/user/contiki-ng/os/dev/nullradio.h:
+/home/user/contiki-ng/os/dev/radio.h:
