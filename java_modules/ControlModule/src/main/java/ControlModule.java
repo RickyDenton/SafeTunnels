89dcafa41ea1273b8ac25d5f3ed2eb24ce60269a
Riccardo Bertini <m_bertini@hotmail.com>
2023-06-09 13:10:36 +0200

    Actuators look ok
    Next: Increase sensors' size

--- java_modules/ControlModule/src/main/java/ControlModule.java
+++ java_modules/ControlModule/src/main/java/ControlModule.java
@@ -1,9 +1,4 @@
-import javax.imageio.ImageIO;
 import javax.swing.*;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
 
 public class ControlModule extends JFrame
@@ -19,49 +14,16 @@ public class ControlModule extends JFrame
   private JPanel logLabelPanel;
   private JPanel devicesListsPanel;
   private JPanel actuatorsListPanel;
-  private JPanel actuator2Panel;
-  private JPanel actuator2HeaderPanel;
-  private JLabel actuator2Name;
-  private JLabel actuator2ConnStateImg;
-  private JPanel actuator2ImagesPanel;
-  private JPanel actuator2FanImagePanel;
-  private JLabel Actuator2FanImageLabel;
-  private JPanel actuator2LightImagePanel;
-  private JLabel actuator2LightImageLabel;
-  private JPanel actuator2CommandsPanel;
-  private JPanel actuator2FanCommandPanel;
-  private JSlider actuator2FanCommandSlider;
-  private JPanel actuator2LightCommandPanel;
-  private JButton actuator2LightCommandButtonOFF;
-  private JButton actuator2LightCommandButtonALERT;
-  private JButton actuator2LightCommandButtonEMERGENCY;
-  private JButton actuator2LightCommandButtonON;
-  private JPanel actuator2ValuesPanel;
-  private JPanel actuator2FanRelSpeedValuePanel;
-  private JLabel actuator2FanRelSpeedValueLabel;
-  private JPanel actuator2LightStateValuePanel;
-  private JLabel actuator2LightStateValueLabel;
   private JPanel actuator1Panel;
   private JPanel actuator1HeaderPanel;
   private JLabel actuator1Name;
   private JLabel actuator1ConnStateImg;
-  private JPanel actuator1ImagesPanel;
-  private JPanel actuator1LightImagePanel;
-  private JLabel actuator1LightImageLabel;
-  private JPanel actuator1CommandsPanel;
-  private JPanel actuator1FanCommandPanel;
-  private JSlider actuator1FanCommandSlider;
-  private JPanel actuator1LightCommandPanel;
-  private JButton actuator1LightCommandButtonOFF;
-  private JButton actuator1LightCommandButtonALERT;
-  private JButton actuator1LightCommandButtonEMERGENCY;
-  private JButton actuator1LightCommandButtonON;
-  private JPanel actuator1ValuesPanel;
-  private JPanel actuator1FanRelSpeedValuePanel;
-  private JLabel actuator1FanRelSpeedValueLabel;
-  private JPanel actuator1LightStateValuePanel;
-  private JLabel actuator1LightStateValueLabel;
   private JPanel sensorsListPanel;
+  private JSlider slider1;
+  private JButton OFFButton;
+  private JButton ONButton;
+  private JButton ALERTButton;
+  private JButton EMERButton;
 
   public ControlModule()
    {
